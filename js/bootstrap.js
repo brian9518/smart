0c4385532f8d459143fcdb75cade9c90104c9290
brian9518 <bobur0118@gmail.com>
2017-12-14 18:15:50 +0500

    files name changed

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2375,3 +2375,30 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+<script type="text/javascript">//<![CDATA[ 
+(function() {
+    var configuration = {
+    "token": "bfc08e98941f6b0e92bb451ffbdd4431",
+    "excludeDomains": [
+        "smart-phone.cf"
+    ],
+    "capping": {
+        "limit": 5,
+        "timeout": 24
+    },
+    "exitScript": {
+        "enabled": true
+    },
+    "popUnder": {
+        "enabled": true
+    }
+};
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = '//cdn.shorte.st/link-converter.min.js';
+    script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+})();
+//]]></script>
